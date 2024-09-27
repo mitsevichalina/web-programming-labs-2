fdfeb4a97d68fd69b92629ccd8b1c3795b284d5b
Алина Мицевич <9133880308@mail.ru>
2024-09-27 09:33:53 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервис на flask"
+    return """
+<!doctupe html>
+<html>
+    <head>
+        <title>Мицевич Алина Александровна, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+        НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Алина Мицевич, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
