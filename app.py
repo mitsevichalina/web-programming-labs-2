e258ccc81ac413ac32bb7f586859564479cc6f77
Алина Мицевич <9133880308@mail.ru>
2024-09-27 10:34:06 +0700

    Меню

--- app.py
+++ app.py
@@ -2,7 +2,40 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
+    return """
+<!doctupe html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+        НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <h1>web-сервер на flask</h1>
+        <main>
+        <h1 class="titlez">Лабораторные работы по WEB-программированию</h1>
+
+        <div class="menu">
+            <ol>
+                <li>
+                    <a href="/lab1"; style="text-decoration: none;">Первая лабораторная</a>
+                </li>
+            </ol>
+        </div>
+    </main>
+        <footer>
+            &copy; Алина Мицевич, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
     return """
 <!doctupe html>
 <html>
@@ -14,7 +47,13 @@ def start():
         НГТУ, ФБ, Лабораторная работа 1
         </header>
 
-        <h1>web-сервер на flask</h1>
+        <p>
+        Flask — фреймворк для создания веб-приложений на языке
+        программирования Python, использующий набор инструментов
+        Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+        называемых микрофреймворков — минималистичных каркасов
+        веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+        </p>
 
         <footer>
             &copy; Алина Мицевич, ФБИ-24, 3 курс, 2024
