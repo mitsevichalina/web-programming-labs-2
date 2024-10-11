2eec71a4e159cfb6ce92d47114cf6de6bd2093a9
Алина Мицевич <9133880308@mail.ru>
2024-10-11 12:56:46 +0700

    Имя студента

--- app.py
+++ app.py
@@ -210,4 +210,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Алина Мицевич'
+    return render_template('example.html', name=name)
