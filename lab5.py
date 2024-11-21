0de3a911626805ed1d26385ab6ec3c44e7cc7ea8
Алина Мицевич <9133880308@mail.ru>
2024-11-22 01:48:31 +0700

    Создание статьи

--- lab5.py
+++ lab5.py
@@ -90,6 +90,25 @@ def list():
     return render_template('lab5/list.html')
 
 
-@lab5.route('/lab5/create')
+@lab5.route('/lab5/create', methods = ['GET', 'POST'])
 def create():
-    return render_template('lab5/create.html')
+    login=session.get('login')
+    if not login:
+        return redirect('/lab5/login')
+    
+    if request.method == 'GET':
+        return render_template('lab5/create_article.html')
+    
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+
+    conn, cur = db_connect()
+
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
+    login_id = cur.fetchone()["id"]
+
+    cur.execute(f"INSERT INTO articles(user_id, title, article_text) \
+                VALUES ({login_id}, '{title}', '{article_text}')")
+
+    db_close(conn, cur)
+    return redirect('/lab5')
