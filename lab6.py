e365dbb0b078d63de229d8da5dee016851815203
Алина Мицевич <9133880308@mail.ru>
2024-12-04 18:59:43 +0700

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -12,7 +12,7 @@ def lab():
     return render_template('lab6/lab6.html')
 
 
-@lab6.route('/lab6/json-rpc-api/', methods=['POST'])
+@lab6.route('/lab6/json-rpc-api/', methods = ['POST'])
 def api():
     data = request.json
     id = data['id']
@@ -22,29 +22,71 @@ def api():
             'result': offices,
             'id': id
         }
+
     login = session.get('login')
-    if not login: 
+    if not login:
         return {
-        'jsonrpc': '2.0', 
-        'error': {
-            'code': 1,
-            'message': 'Unauthorized'
-        },
-        'id': id
-    }
+            'jsonrpc': '2.0',
+            'error': {
+                'code': 1,
+                'message': 'Unauthorized'
+            },
+            'id': id
+        }
+
     if data['method'] == 'booking':
         office_number = data['params']
         for office in offices:
             if office['number'] == office_number:
                 if office['tenant'] != '':
                     return {
-                        'jsonrpc': '2.0', 
-                        'result': '2.0',
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 2,
+                            'message': 'Already booked'
+                        },
                         'id': id
                     }
-                
+
+                office['tenant'] = login
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }
+            
+    if data['method'] == 'cancellation':
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+                if office['tenant'] == '':
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 3,
+                            'message': 'Office is not rented'
+                        },
+                        'id': id
+                    }
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 4,
+                            'message': 'You cannot rent a "foreign" lease'
+                        },
+                        'id': id
+                    }
+                office['tenant'] = ''
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }
+
+
     return {
-        'jsonrpc': '2.0', 
+        'jsonrpc': '2.0',
         'error': {
             'code': -32601,
             'message': 'Method not found'
