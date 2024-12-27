e76979fba5282f3ee1eca2a26d27ab6f66d7e873
Алина Мицевич <9133880308@mail.ru>
2024-12-27 18:39:01 +0700

    регистрация

--- app.py
+++ app.py
@@ -27,11 +27,12 @@ if app.config['DB_TYPE'] == 'postgres':
     host_ip = '127.0.0.1'
     host_port = 5432
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
 else:
     dir_path = path.dirname(path.realpath(__file__))
     db_path = path.join(dir_path, "alina_mitsevich_orm.db")
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///alina_mitsevich_orm.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 
 db.init_app(app)
 
